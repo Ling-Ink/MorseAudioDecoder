9fd68a507667c32b73d3a0114f3e0048dbc5e488
Moling <clovermoling@gmail.com>
2023-06-12 13:46:10 +0800

    Update

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ morse_dict = {
     '.--.-.': '@'
 }
 
-pylab.figure(dpi=200, figsize=(10, 2))  # 将显示的所有图分辨率调高
-
 # 加载音频
 audio = wave.open(sys.argv[1], 'rb')
 
@@ -31,6 +29,9 @@ audio = wave.open(sys.argv[1], 'rb')
 params = audio.getparams()
 print(params)
 n_channels, _, sample_rate, n_frames = params[:4]
+
+# 将显示的所有图分辨率调高
+pylab.figure(dpi=200, figsize=(1000000 / n_frames * 50, 2))
 
 # 读频谱信息
 str_wave_data = audio.readframes(n_frames)
@@ -40,8 +41,8 @@ audio.close()
 wave_data = np.frombuffer(str_wave_data, dtype=np.short).T
 
 # 计算平均频率
-wave_avg = int(sum([abs(x / 10) for x in wave_data]) / len(wave_data))
-print(wave_avg)
+wave_avg = int(sum([abs(x / 10) for x in wave_data]) / len(wave_data)) * 10
+print("wave avg: " + str(wave_avg))
 
 # 绘制摩斯图像
 morse_block_sum = 0  # 待划分的数据
@@ -84,12 +85,15 @@ for i in morse_arr:
         morse_type.append(i)
         morse_len.append(1)
     else:
-        morse_obj_sum[i] += 1
-        morse_len[len(morse_type) - 1] += 1
+        if morse_len[len(morse_type) - 1] <= 100:
+            morse_obj_sum[i] += 1
+            morse_len[len(morse_type) - 1] += 1
 
 # 计算信息与空位的平均长度
 morse_block_avg = morse_obj_sum[1] / morse_obj_len[1]
+print("morse block avg: " + str(morse_block_avg))
 morse_blank_avg = morse_obj_sum[0] / morse_obj_len[0]
+print("morse blank avg: " + str(morse_blank_avg))
 
 # 转换为摩斯电码
 morse_result = ""
@@ -106,11 +110,12 @@ for i in range(len(morse_type)):
         if morse_len[i] > morse_blank_avg:
             morse_result += "/"
 
+print("Morse Result: " + morse_result)
+
 # 摩斯电码解码
 morse_array = morse_result.split("/")
 plain_text = ""
 for morse in morse_array:
-    plain_text += morse_dict[morse]
-
-print("Morse Result: " + morse_result)
+    if morse != '':
+        plain_text += morse_dict[morse]
 print("Plain Text: " + plain_text)
